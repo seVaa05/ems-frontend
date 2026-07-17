74a6e38d31a49d703dcda5d29a858c638f9490a0
Lazar Sevic <lazarsevic05@gmail.com>
2026-07-17 14:26:35 +0200

    Add header and footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import './App.css'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
 import HelloWorld from './HelloWorld'
+import HeaderComponent from './components/HeaderComponent'
+import FooterComponent from './components/FooterComponent'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
 function App() {
+  
 
   return (
     <>
-      <ListEmployeeComponent/>
+      <BrowserRouter>
+        <HeaderComponent/>
+          <Routes>
+            /* http://localhost:3000 */
+            <Route path='/' element={<ListEmployeeComponent/>}></Route>
+            <Route path='/employees' element={<ListEmployeeComponent/>}></Route>
+          </Routes>
+
+        <FooterComponent/>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/components/HeaderComponent.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const HeaderComponent = () => {
+  return (
+    <div>
+        <header>
+            <nav className="navbar navbar-dark bg-dark">
+                <a class="navbar-brand" href="https://www.javaguides.net">Employee Managment System</a>
+            </nav>
+        </header>
+
+    </div>
+  )
+}
+
+export default HeaderComponent
