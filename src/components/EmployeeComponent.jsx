7ca92c3e5acb32ea1b3e66bde3a5f493ed8351fe
Lazar Sevic <lazarsevic05@gmail.com>
2026-07-17 18:01:25 +0200

    finished

--- src/components/EmployeeComponent.jsx
+++ src/components/EmployeeComponent.jsx
@@ -1,4 +1,6 @@
-import React, {useState}  from 'react'
+import React, {useState, useEffect}  from 'react'
+import { createEmployee, getEmployee, updateEmployee } from '../services/EmployeeService';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const EmployeeComponent = () => {
   
@@ -7,40 +9,134 @@ const EmployeeComponent = () => {
     const [email,setEmail] = useState('');
   
 
-    function saveEmployee(e){
+    const {id} = useParams();
+    const[errors,setErrors]=useState({
+        firstName: '',
+        lastName: '',
+        email: ''
+    })
+
+    const navigator = useNavigate();
+
+    useEffect(() => {
+
+        if(id){
+            getEmployee(id).then((response) => {
+                setFirstName(response.data.firstName);
+                setLastName(response.data.lastName);
+                setEmail(response.data.email)
+            }).catch(error =>{
+                console.error(error)
+            })
+        }
+
+    },[id])
+
+
+    function saveOrUpdateEmployee(e){
         e.preventDefault();
 
-        const employee = {firstName, lastName, email}
-        console.log(employee);
+        if(validateForm()){
+            const employee = {firstName, lastName, email}
+            console.log(employee);
+
+            if(id){
+                updateEmployee(id,employee).then((response)=>{
+                    console.log(response.data);
+                    navigator('/employees');
+                }).catch(error=>{
+                    console.error(error);
+                })
+            }
+            else{
+                createEmployee(employee).then((response) => {
+                    console.log(response.data);
+                    navigator('/employees')
+                }).catch(error =>{
+                    console.log(error);
+                }) 
+            }
+        }
+
     }
   
+    function validateForm(){
+        let valid = true;
+
+        const errorsCopy = {...errors}
+
+        if(firstName.trim()){
+            errorsCopy.firstName = '';
+        }else{
+            errorsCopy.firstName = 'First name is required';
+            valid = false;
+        }
+
+        if(lastName.trim()){
+            errorsCopy.lastName = '';
+        }else{
+            errorsCopy.lastName='Last name is required';
+            valid = false;
+        }
+
+        if(email.trim()){
+            errorsCopy.email='';
+        }
+        else{
+            errorsCopy.email='Email is required';
+            valid = false;
+        }
+
+        setErrors(errorsCopy);
+
+        return valid;
+
+    }
+    
+    function pageTitle(){
+        if(id){
+            return <h2 className='text-center'>Update Employee</h2>
+        }
+        else{
+            return <h2 className='text-center'>Add Employee</h2>
+        }
+    }
+
+
     return (
     <div className='container'>
         <br /><br />
         <div className='row'>
             <div className='card col-md-6 offset-md-3 offset-md-3'>
-                <h2 className='text-center'>Add Employee</h2>
+                {
+                    pageTitle()
+                }
                 <div className='card-body'>
                     <form action="">
                         <div className='form-group mb-2'>
                             <label children='form-label'>Employee First Name:</label>
                             <input type="text" placeholder='Enter Employee First Name'
                              name='firstName' value={firstName}
-                              className='form-control' onChange={(e) =>setFirstName(e.target.value)}
+                              className={`form-control ${errors.firstName ? 'is-invalid':''}`} onChange={(e) =>setFirstName(e.target.value)}
                                />
+                               {errors.firstName && <div className='invalid-feedback'>{errors.firstName}</div>}
                             <label children='form-label'>Employee Last Name:</label>
                             <input type="text" placeholder='Enter Employee Last Name'
                              name='lastName' value={lastName}
-                              className='form-control' onChange={(e) =>setLastName(e.target.value)}
+                              className={`form-control ${errors.lastName ? 'is-invalid':''}`} onChange={(e) =>setLastName(e.target.value)}
                                />
+                                {errors.lastName && <div className='invalid-feedback'>{errors.lastName}</div>}
                             <label children='form-label'>Employee Email:</label>
                             <input type="text" placeholder='Enter Employee Email'
                              name='email' value={email}
-                              className='form-control' onChange={(e) =>setEmail(e.target.value)}
+                             className={`form-control ${errors.email ? 'is-invalid':''}`} 
+                              onChange={(e) =>setEmail(e.target.value)}
                                />
+                                {errors.email && <div className='invalid-feedback'>{errors.email}</div>}
+
                         </div>
 
-                        <button className='btn btn-success' onClick={saveEmployee}>Submit</button>
+                        <button className='btn btn-success' onClick={saveOrUpdateEmployee}>Submit</button>
                     </form>
                 </div>
             </div>
--- src/components/ListEmployeeComponent.jsx
+++ src/components/ListEmployeeComponent.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect,useState} from 'react'
-import { listEmployees } from '../services/EmployeeService'
+import { deleteEmployee, listEmployees } from '../services/EmployeeService'
 import {useNavigate} from 'react-router-dom'
+
 
 const ListEmployeeComponent = () => {
 
@@ -9,16 +10,36 @@ const ListEmployeeComponent = () => {
 
 
     useEffect(() => {
+        getAllEmployees();
+    },[])
+
+
+    function getAllEmployees(){
         listEmployees().then((response) => {
             setEmployees(response.data);
         }).catch(error =>{
             console.error(error)
         })
-    },[])
-
+    }
 
     function addNewEmployee(){
         navigator('/add-employee')
+    }
+
+
+    function updateEmployee(id){
+        navigator(`/edit-employee/${id}`)
+    }
+
+
+    function removeEmployee(id){
+        console.log(id);
+
+        deleteEmployee(id).then((response)=>{
+            getAllEmployees();
+        }).catch(error=>{
+            console.error(error);
+        })
     }
 
     return (
@@ -42,6 +63,10 @@ const ListEmployeeComponent = () => {
                             <td>{employee.firstName}</td>
                             <td>{employee.lastName}</td>
                             <td>{employee.email}</td>
+                            <td>
+                                <button className='btn btn-info' onClick={()=>updateEmployee(employee.id)}>Update</button>
+                                <button className='btn btn-danger' onClick = {()=>removeEmployee(employee.id)}>Delete</button>
+                            </td>
                         </tr>
                     )
                 }
